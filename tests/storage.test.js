461dcd622696401f9ce41fea70e5cfc851202e6c
Brett Adams <brettadams@fastmail.com>
2026-05-30 11:48:43 -0400

    chore: drop unused assertTrue import in storage tests

--- tests/storage.test.js
+++ tests/storage.test.js
@@ -1,4 +1,4 @@
-import { describe, test, assertEq, assertTrue } from './runner.js';
+import { describe, test, assertEq } from './runner.js';
 import * as Storage from '../model/storage.js';
 import * as T from '../model/template.js';
 
